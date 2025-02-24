fc9d227b9b33206ae59b8f84d76f5ee0cd3e2ea3
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-02-24 18:24:10 +0600

    images added and make responsive

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 document.getElementById('ol-list')
-.addEventListener('click', function (event) {
-    if (event.target.tagName === 'LI') {
-        event.target.remove();
-    }
-});
+    .addEventListener('click', function (event) {
+        if (event.target.tagName === 'LI') {
+            event.target.remove();
+        }
+    });
 
 // Add new item to the list
 document.getElementById('btn-add')
-.addEventListener('click', function () {
-    const input = document.getElementById('input-item');
-    const newItemText = input.value.trim();
-    if (newItemText) {
-        const ol = document.getElementById('ol-list');
-        const li = document.createElement('li');
-        li.classList.add('text-xl', 'text-gray-800', 'p-2', 'rounded-lg', 'cursor-pointer');
-        li.textContent = newItemText;
-        ol.appendChild(li);
-        input.value = ''; // Clear the input field
-    }
-});
+    .addEventListener('click', function () {
+        const input = document.getElementById('input-item');
+        const newItemText = input.value.trim();
+        if (newItemText) {
+            const ol = document.getElementById('ol-list');
+            const li = document.createElement('li');
+            li.classList.add('text-xl', 'text-gray-800', 'p-2', 'rounded-lg', 'cursor-pointer');
+            li.textContent = newItemText;
+            ol.appendChild(li);
+            input.value = ''; // Clear the input field
+        }
+    });
